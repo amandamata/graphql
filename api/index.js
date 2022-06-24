9eefb693cd9bc7e47082da9a33cc8881fac50f17
amandamata <amanda.mata@pagar.me>
2022-06-24 07:28:11 -0300

    :construction: user schema

--- api/index.js
+++ api/index.js
@@ -1,2 +1,20 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
+const userSchema = require('./user/schema/user.graphql')
+
+const users = [
+    {
+        name: "Amanda",
+        active: true
+    },
+    {
+        name: "Marcia",
+        active: false
+    }
+]
+
+const typeDefs = [userSchema]
+const resolvers = []
 const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => {
+    console.log(`Server running at ${ url }`)
+}) 
