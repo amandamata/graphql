5d1481e16c9c7f76f0abe22863a752f5b6148b60
amandamata <amanda.mata@pagar.me>
2022-06-24 09:34:50 -0300

    :construction: merging endpoints

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -6,11 +6,20 @@ class UsersApi extends RESTDataSource {
         this.baseURL = 'http://localhost:3000'
     }
     async getUsers(){
-        return this.get('/users')
+        const users = await this.get('/users')
+        return users.map(async user => ({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            active: user.active,
+            role: await this.get(`/roles/${user.role}`)
+        }))
     }
 
     async getUserById(id){
-        return this.get(`/users/${id}`)
+        const user = await this.get(`/users/${id}`)
+        user.role = await this.get(`/roles/${user.role}`)
+        return user
     }
 }
 
